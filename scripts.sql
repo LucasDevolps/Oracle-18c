a0346ed7775a6f6025638bb29c8c90ac37c68fb2
Lucas DevOps <51280960+LucasDevolps@users.noreply.github.com>
2021-09-14 11:27:37 -0300

    Primeiro bloco anonimo

---
+++ scripts.sql
@@ -0,0 +1,14 @@
+SET SERVEROUTPUT ON
+DECLARE
+    /*
+        ':=' Inicializa váriaveis
+    */
+
+    vTexto VARCHAR2(100) := 'Seja bem vindo ao PL/SQL';
+BEGIN
+    DBMS_OUTPUT.PUT_LINE(vTexto);
+EXCEPTION
+    WHEN OTHERS
+    THEN
+        DBMS_OUTPUT.PUT_LINE('Erro Oracle ' || SQLCODE || SQLERRM);
+END;
