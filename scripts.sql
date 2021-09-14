0677992f0f73a344d6e423ad356a4ea7dc96c69f
Lucas DevOps <51280960+LucasDevolps@users.noreply.github.com>
2021-09-14 14:14:31 -0300

    Atualizando repositório

--- scripts.sql
+++ scripts.sql
@@ -2,6 +2,10 @@ SET SERVEROUTPUT ON
 DECLARE
     /*
         ':=' Inicializa váriaveis
+         'DBMS_OUTPUT.PUT_LINE' imprime algo no console
+         'SQLCODE' mostra o código do erro
+         'SQLERRM' mostra a descrição do erro
+        
     */
 
     vTexto VARCHAR2(100) := 'Seja bem vindo ao PL/SQL';
@@ -12,3 +16,49 @@ EXCEPTION
     THEN
         DBMS_OUTPUT.PUT_LINE('Erro Oracle ' || SQLCODE || SQLERRM);
 END;
+
+----------------------------------------------------------
+--Aula 08
+
+SET SERVEROUTPUT ON
+DECLARE
+    vNumero1  NUMBER(11,2) := 500;
+    vNumero2  NUMBER(11,2) := 400;
+    vMedia NUMBER(11,2);
+BEGIN 
+    vMedia := (vNumero1 + vNumero2) / 2;
+    DBMS_OUTPUT.PUT_LINE('Media = ' || vMedia);
+END;
+
+----------------------------------------------------------
+--Aula 09
+
+SET SERVEROUTPUT ON
+DECLARE 
+    vNumero number(11,2) := 1200.50;
+BEGIN
+    DBMS_OUTPUT.PUT_LINE('Numero = ' || vNumero);
+END;
+
+-----
+
+DECLARE
+    vCaracterTamFixo char(2) := 'RS';
+    vCaracterTamVariavel varchar2(100) := 'Porto Alegre, RS';
+BEGIN
+    DBMS_OUTPUT.PUT_LINE('String Caracteres Tam Fixo = ' || vCaracterTamFixo);
+    DBMS_OUTPUT.PUT_LINE('String Caracteres Tam Váriavel = ' || vCaracterTamVariavel);
+END;
+
+----
+
+DECLARE 
+    vData1 Date := '29/04/20';
+    vData2 Date := '29/04/2020';
+BEGIN
+    DBMS_OUTPUT.PUT_LINE('Data1 = ' || vData1);
+    DBMS_OUTPUT.PUT_LINE('Data2 = ' || vData2);
+END;
+
+----------------------------------------------------------
+--Aula 10
