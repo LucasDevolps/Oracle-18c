88aac522579b534943e3d9e4ebb8d1de27259366
Lucas DevOps <51280960+LucasDevolps@users.noreply.github.com>
2021-09-21 19:11:16 -0300

    Variáveis dentro do select pl/sql

--- scripts.sql
+++ scripts.sql
@@ -177,3 +177,96 @@ BEGIN
     
     DBMS_OUTPUT.PUT_LINE('Referencia variável Bloco 1: ' || vnumero1);
 END;
+
+
+----------------------------------------------------------
+--Aula 18
+
+SET SERVEROUTPUT ON 
+DECLARE
+    vFirst_name   employees.first_name%type;
+    vLast_name    employees.last_name%type;
+    vSalary       employees.salary%type;
+    vEmployee_id  employees.employee_id%type := 100;
+BEGIN 
+    SELECT
+        first_name, last_name, salary
+    INTO 
+        vFirst_name, vLast_name, vSalary
+    FROM 
+        employees
+    WHERE 
+        employee_id = vEmployee_id;
+     DBMS_OUTPUT.PUT_LINE('Employee Id: ' || vEmployee_id);
+     DBMS_OUTPUT.PUT_LINE('First Name: ' || vFirst_name);     
+     DBMS_OUTPUT.PUT_LINE('Last Name: ' || vLast_name);     
+     DBMS_OUTPUT.PUT_LINE('Salary: ' || vSalary);     
+END;
+
+-- Erro ORA-01403 - No data Found
+
+SET SERVEROUTPUT ON 
+DECLARE
+    vFirst_name   employees.first_name%type;
+    vLast_name    employees.last_name%type;
+    vSalary       employees.salary%type;
+    vEmployee_id  employees.employee_id%type := 230;
+BEGIN 
+    SELECT
+        first_name, last_name, salary
+    INTO 
+        vFirst_name, vLast_name, vSalary
+    FROM 
+        employees
+    WHERE 
+        employee_id = vEmployee_id;
+     DBMS_OUTPUT.PUT_LINE('Employee Id: ' || vEmployee_id);
+     DBMS_OUTPUT.PUT_LINE('First Name: ' || vFirst_name);     
+     DBMS_OUTPUT.PUT_LINE('Last Name: ' || vLast_name);     
+     DBMS_OUTPUT.PUT_LINE('Salary: ' || vSalary);     
+END;
+
+DECLARE
+    vJob_id     employees.job_id%type := 'IT_PROG';
+    vAvg_salary employees.salary%type;
+    vSum_salary employees.salary%type;
+BEGIN 
+    SELECT 
+        ROUND(AVG(salary),2),
+        ROUND(SUM(salary),2)
+    INTO
+        vAvg_salary,
+        vSum_salary
+    FROM 
+        employees
+    WHERE 
+        job_id = vJob_id;
+    DBMS_OUTPUT.PUT_LINE('Cargo: ' || vJob_id);
+    DBMS_OUTPUT.PUT_LINE('Média: ' || vAvg_salary);
+    DBMS_OUTPUT.PUT_LINE('Somatório: ' || vSum_salary);
+END;
+
+-- to many row exception
+
+
+SET SERVEROUTPUT ON 
+DECLARE
+    vJob_id     employees.job_id%type;
+    vAvg_salary employees.salary%type;
+    vSum_salary employees.salary%type;
+BEGIN 
+    SELECT 
+        ROUND(AVG(salary),2),
+        ROUND(SUM(salary),2)
+    INTO
+        vJob_id,
+        vAvg_salary,
+        vSum_salary
+    FROM 
+        employees
+    group by job_id;
+    DBMS_OUTPUT.PUT_LINE('Cargo: ' || vJob_id);
+    DBMS_OUTPUT.PUT_LINE('Média: ' || vAvg_salary);
+    DBMS_OUTPUT.PUT_LINE('Somatório: ' || vSum_salary);
+END;
+
