32da182c0fa3a58df8b2566be901851d9767eb5f
Lucas DevOps <51280960+LucasDevolps@users.noreply.github.com>
2021-10-03 14:39:15 -0300

    Atualizando repo

--- scripts.sql
+++ scripts.sql
@@ -270,3 +270,25 @@ BEGIN
     DBMS_OUTPUT.PUT_LINE('Somatório: ' || vSum_salary);
 END;
 
+----------------------------------------------------------
+--Aula 19
+
+
+SET SERVEROUTPUT ON
+DECLARE
+    vfirst_name employees.first_name%type;
+    vlast_name  employees.last_name%type;
+    vsalary     employees.salary%type;
+BEGIN
+    INSERT INTO employees
+    (employee_id,first_name, last_name, email, phone_number, hire_date, 
+        job_id, salary, commission_pct, manager_id, department_id
+    )
+    VALUES
+    (
+        employees_seq.nextval, 'Kobe', 'Bryant', 'KBRYANT', '55.123.45568', SYSDATE,
+        'IT_PROG', 15000,0.4,103,60
+    );
+    COMMIT;
+END;
+
