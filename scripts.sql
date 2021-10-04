bba338424b8c0c03c19c4d14ddf4c0bd08e6fce4
Lucas DevOps <51280960+LucasDevolps@users.noreply.github.com>
2021-10-04 20:41:25 -0300

    comando case

--- scripts.sql
+++ scripts.sql
@@ -376,3 +376,37 @@ BEGIN
     WHERE department_id = vDepartment_id;
     COMMIT;
 END;
+
+----------------------------------------------------------
+--Aula 26 - Comando CASE
+
+SET SERVEROUTPUT ON
+ACCEPT pdepartment_id PROMPT 'Digite o Id do departamento: '
+DECLARE
+    vpercentual     number(3);
+    vDepartment_id  employees.employee_id%type := &pdepartment_id;
+BEGIN
+    CASE vDepartment_id
+    
+    WHEN 80 
+    THEN vpercentual := 10; -- Sales
+    
+    WHEN 20
+    THEN vpercentual := 15; --Marketing
+    
+    WHEN 60
+    THEN vpercentual := 20; --TI
+    
+    ELSE vpercentual := 5;
+    
+    END CASE;
+    DBMS_OUTPUT.PUT_LINE('Id do Departamento: '|| &pdepartment_id);
+    DBMS_OUTPUT.PUT_LINE('Percentual: '|| vpercentual);
+    
+    
+    UPDATE employees
+    SET salary = salary * (1 + vpercentual / 100)
+    WHERE department_id = &pdepartment_id;
+    --COMMIT;
+END;
+
