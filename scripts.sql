0b565598e25d8466ecc049e521b30a3edd8613e9
Lucas DevOps <51280960+LucasDevolps@users.noreply.github.com>
2021-10-27 15:12:41 -0300

    Pragma exception init

--- scripts.sql
+++ scripts.sql
@@ -939,21 +939,41 @@ BEGIN
         RAISE ePresident
     END IF;
     
-    EXCEPTION
-        WHEN NO_DATA_FOUND THEN
-         RAISE_APPLICATION_ERROR(-20001, 'Empregado não encontrado, id = '
-         || TO_CHAR(vEmployee_id)
-         );
-        WHEN ePresident THEN
-            UPDATE employees
-            SET salary = salary * 1.5
-            WHERE employee_id = vEmployee_id;
-            --COMMIT;
-        WHEN OTHERS THEN
-            RAISE_APPLICATION_ERROR(-20002, 'Erro Oracle ' || SQLCODE || SQLERRM);
+EXCEPTION
+    WHEN NO_DATA_FOUND THEN
+     RAISE_APPLICATION_ERROR(-20001, 'Empregado não encontrado, id = '
+     || TO_CHAR(vEmployee_id)
+     );
+    WHEN ePresident THEN
+        UPDATE employees
+        SET salary = salary * 1.5
+        WHERE employee_id = vEmployee_id;
+        --COMMIT;
+    WHEN OTHERS THEN
+        RAISE_APPLICATION_ERROR(-20002, 'Erro Oracle ' || SQLCODE || SQLERRM);
 END;
  
+----------------------------------------------------------
+--Aula 46 - Pragma Exception Init
 
+DECLARE
+    vFirst_name   employees.first_name%Type := 'Robert';
+    vLast_name    employees.last_name%Type  := 'Ford';
+    vJob_id       employees.job_id%Type := 'XX_YYYY';
+    vPhone_number employees.phone_number%Type := '650.511.9844';
+    vEmail        employees.email%Type := 'RFORD';
+    eFk_inexistente  EXCEPTION;
+    PRAGMA EXCEPTION_INIT(eFk_inexistente,-2291);
+BEGIN
+    INSERT INTO employees (employee_id, first_name, last_name, phone_number, email, hire_date, job_id)
+    VALUES (employees_seq.nextval, vfirst_name, vlast_name, vPhone_number, vemail, sysdate, vjob_id);
+    COMMIT;
+EXCEPTION   
+    WHEN eFk_inexistente THEN
+        RAISE_APPLICATION_ERROR(-20003, 'Job inexistente!');
+    WHEN OTHERS THEN
+        RAISE_APPLICATION_ERROR(-20002, 'Erro Oracle' || SQLCODE || SQLERRM);
+END;
 
 
 
