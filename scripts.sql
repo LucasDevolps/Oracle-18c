8be12c30c38f9c1d864ccbfe9751ece44d45047f
Lucas DevOps <51280960+LucasDevolps@users.noreply.github.com>
2021-10-28 10:50:38 -0300

    Criando Procedures de banco

--- scripts.sql
+++ scripts.sql
@@ -975,6 +975,71 @@ EXCEPTION
         RAISE_APPLICATION_ERROR(-20002, 'Erro Oracle' || SQLCODE || SQLERRM);
 END;
 
+----------------------------------------------------------
+--Aula 47 - Criando Procedures de banco 
+
+
+CREATE OR REPLACE PROCEDURE PRC_INSERE_EMPREGADO
+    (
+        pfirst_name     IN VARCHAR2,
+        plast_name      IN VARCHAR2,
+        pemail          IN VARCHAR2,
+        pphone_number   IN VARCHAR2,
+        phire_date      IN DATE DEFAULT SYSDATE,
+        pjob_id         IN VARCHAR2,
+        pSALARY         IN NUMBER,
+        pCOMMICION_PCT  IN NUMBER, 
+        pMANAGER_ID     IN NUMBER,
+        pDEPARTMENT_ID  IN NUMBER
+    )
+IS
+    --Nenhuma váriavel declarada
+BEGIN
+    INSERT INTO employees(
+        employee_id,
+        first_name,
+        last_name,
+        email,
+        phone_number,
+        hire_date,
+        job_id,
+        salary,
+        commission_pct,
+        manager_id,
+        department_id        
+    )
+    VALUES
+    (
+        EMPLOYEES_SEQ.nextval,
+        pfirst_name,
+        plast_name,
+        pemail,
+        pphone_number,
+        phire_date,
+        pjob_id,
+        psalary,
+        pcommicion_pct,
+        pMANAGER_ID,
+        pdepartment_id
+    );
+EXCEPTION
+    WHEN OTHERS THEN 
+        RAISE_APPLICATION_ERROR(-20001, 'Erro Oracle' || SQLCODE || SQLERRM);
+END; 
+
+-- Executando a procedure pelo Bloco PL/SQL
+
+BEGIN
+    PRC_INSERE_EMPREGADO('David','Bowie', 'DBOWIE', '515.127.4861', 
+    SYSDATE, 'IT_PROG', 1500,NULL, 103,60);
+    COMMIT;
+END;
+
+-- Executando procedure com o comando EXECUTE do SQL*PUBLUS
+
+EXEC PRC_INSERE_EMPREGADO('Greg','Lake', 'GLAKE', '515.127.4961',SYSDATE,'IT_PROG', 15000, NULL,103,60);
+
+COMMIT;
 
 
 
